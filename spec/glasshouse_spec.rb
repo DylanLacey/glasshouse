5bce2487c1e9f6a465a545b6a9c25efec36835af
Dylan Lacey <github@dylanlacey.com>
2016-01-31 22:36:58 -0800

    Added #grow and #prune

--- spec/glasshouse_spec.rb
+++ spec/glasshouse_spec.rb
@@ -5,7 +5,39 @@ describe Glasshouse do
     expect(Glasshouse::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#grow' do
+    it "adds a value to the Env Vars during a block" do
+      Glasshouse.grow({"bulb" => "tulip"}) do
+        expect(ENV['bulb']).to match "tulip"
+      end
+    end
+
+    it "removes the value after the block" do
+      Glasshouse.grow({"sprout" => "bean"}) do
+        #null op
+      end
+
+      expect(ENV['sprout']).to be_nil
+    end
+  end
+
+  describe "#prune" do
+    before do
+      ENV["roses"] = "thorny"
+    end
+
+    it "removes a value from the Env Vars during a block" do
+      Glasshouse.prune(["roses"]) do
+        expect(ENV["roses"]).to be nil
+      end
+    end
+
+    it "restores the value after the block" do
+      Glasshouse.prune(["roses"]) do
+        #null op
+      end
+
+      expect(ENV["roses"]).to match "thorny"
+    end
   end
 end
